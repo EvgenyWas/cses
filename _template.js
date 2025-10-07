8a3664ba624173cfbe05999de7ca408e6ea6f93a
Yauheni Vasiukevich <evgeniywas@gmail.com>
2025-10-07 22:00:10 +0100

    update solution template

--- _template.js
+++ _template.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
-const input = fs.readFileSync(0, "utf8").trim().split("\n");
-const output = [];
+const [input] = fs.readFileSync(0, "utf8").trim().split("\n");
 
-console.log(output.join("\n"));
+console.log(getSolution(input));
+
+function getSolution() {}
