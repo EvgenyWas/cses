53df181180b5818d587291544650cde9b42a6656
Yauheni Vasiukevich <evgeniywas@gmail.com>
2025-11-15 20:01:25 +0000

    refactor: update Ferris Wheel problem solution

--- sort-and-search/ferris-wheel.js
+++ sort-and-search/ferris-wheel.js
@@ -8,32 +8,19 @@ console.log(getFerrisWheel());
 
 function getFerrisWheel() {
   let count = 0;
-  const search = (arr) => {
-    if (!arr.length) {
-      return;
+  let left = 0;
+  let right = kidsWeights.length - 1;
+  kidsWeights.sort((a, b) => a - b);
+  while (left <= right) {
+    const lightest = kidsWeights[left];
+    const heaviest = kidsWeights[right];
+    if (maxWeight - heaviest >= lightest) {
+      left++;
     }
 
-    if (arr.length === 1) {
-      count++;
-      return;
-    }
-
-    const sortedArr = arr.sort((a, b) => a - b);
-    const middle = Math.floor(sortedArr.length / 2);
-    const left = sortedArr.slice(0, middle).reverse();
-    const right = sortedArr.slice(middle);
-    while (right.length) {
-      const heaviest = right.pop();
-      const lightest = left[left.length - 1];
-      if (maxWeight - heaviest >= lightest) {
-        left.pop();
-      }
-
-      count++;
-    }
-    search(left);
-  };
-  search(kidsWeights);
+    right--;
+    count++;
+  }
 
   return count;
 }
